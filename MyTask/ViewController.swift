def05cd4bac1811d5272ea732580828a250ead72
Nikolai Eremenko <nikolai.dev@icloud.com>
2024-12-25 18:17:10 +0400

    Initial Commit

---
+++ MyTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTask
+//
+//  Created by Nikolai Eremenko on 25.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
